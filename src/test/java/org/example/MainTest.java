9089139aa3b8bfc1f8854ffd6ab14063f477bb1e
xuancanhit <xuancanhit99@gmail.com>
2024-05-17 19:57:56 +0300

    Add Unit Test

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,7 +1,15 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest {
 
@@ -76,4 +84,58 @@ public class MainTest {
         String actual = Main.formatPhoneNumber(input);
         assertEquals(expected, actual);
     }
+
+//    @Test
+//    public void testMainWithValidFile() throws IOException {
+//        // Create a temporary file with test content
+//        String testContent = "+1 (123) 456-78-90\n+7 123 456-78-90\n+44 123 456 78 90";
+//        Files.write(Paths.get("src/main/java/org/example/test.txt"), testContent.getBytes());
+//
+//        // Redirect standard output to capture the output of the main method
+//        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+//        System.setOut(new PrintStream(outContent));
+//
+//        // Run the main method
+//        Main.main(new String[]{});
+//
+//        // Check the output
+//        String expectedOutput = "+1 (123) 456-78-90\n+7 (123) 456-78-90\n+44 (123) 456-78-90\n";
+//        assertEquals(expectedOutput, outContent.toString());
+//
+//        // Clean up the temporary file
+//        Files.delete(Paths.get("src/main/java/org/example/test.txt"));
+//    }
+
+//    @Test
+//    public void testMainWithEmptyFile() throws IOException {
+//        // Create a temporary empty file
+//        Files.createFile(Paths.get("src/main/java/org/example/test.txt"));
+//
+//        // Redirect standard output to capture the output of the main method
+//        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+//        System.setOut(new PrintStream(outContent));
+//
+//        // Run the main method
+//        Main.main(new String[]{});
+//
+//        // Check the output
+//        String expectedOutput = "";
+//        assertEquals(expectedOutput, outContent.toString());
+//
+//        // Clean up the temporary file
+//        Files.delete(Paths.get("src/main/java/org/example/test.txt"));
+//    }
+
+//    @Test
+//    public void testMainWithNonExistentFile() {
+//        // Redirect standard error to capture the error output of the main method
+//        ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+//        System.setErr(new PrintStream(errContent));
+//
+//        // Run the main method with a non-existent file path
+//        Main.main(new String[]{"non_existent_file.txt"});
+//
+//        // Check the error output
+//        assertTrue(errContent.toString().contains("Error reading the file"));
+//    }
 }
