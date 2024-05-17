9782c32bec1308134934dbf20f3d73d3e97249fe
xuancanhit <xuancanhit99@gmail.com>
2024-05-17 18:59:21 +0300

    Add more Unit Test

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -20,4 +20,53 @@ public class MainTest {
         String actual = Main.formatPhoneNumber(input);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testFormatPhoneNumberWithNoSpaces() {
+        String input = "+71234567890";
+        String expected = "+1 (123) 456-78-90";
+        String actual = Main.formatPhoneNumber(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFormatPhoneNumberWithDashes() {
+        String input = "+1-123-456-78-90";
+        String expected = "+1 (123) 456-78-90";
+        String actual = Main.formatPhoneNumber(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFormatPhoneNumberWithBrackets() {
+        String input = "+1 (123) 456-78-90";
+        String expected = "+1 (123) 456-78-90";
+        String actual = Main.formatPhoneNumber(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFormatPhoneNumberWithSpaces() {
+        String input = "+1 123 456 78 90";
+        String expected = "+1 (123) 456-78-90";
+        String actual = Main.formatPhoneNumber(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFormatPhoneNumberWithCountryCode() {
+        String input = "+44 123 456 78 90";
+        String expected = "+1 (412) 345-67-890";
+        String actual = Main.formatPhoneNumber(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFormatPhoneNumberWithShortNumber() {
+        String input = "1234567890";
+        String expected = "+1 (234) 567-89-0";
+        String actual = Main.formatPhoneNumber(input);
+        assertEquals(expected, actual);
+    }
+
 }
